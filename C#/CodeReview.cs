b989b3e8cd2ce81a639693527b7e9c41bafa49d8
Raymond Yuan <yuanqingxiao@outlook.com>
2022-03-11 21:08:46 -0800

    Update CodeReview.cs

--- C#/CodeReview.cs
+++ C#/CodeReview.cs
@@ -1,18 +1,31 @@
-for (stat 1; stat 2; stat 3) 
-{
-  // stat execute once only
-  // define array method 1
-  string[] cars;
-  
-  // Add values, must use new
-  cars = new string[] {"Volvo", "BMW", "Ford"};
-  //method2
-  string[] cars1 = {"Volvo", "BMW", "Ford", "Mazda"};
-  
-  // loop usage
-  foreach (string i in cars) 
-  {
-    Console.WriteLine(i);
-  }
+using System;
 
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!aa");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            // stat execute once only
+            // define array method 1
+            string[] cars;
+
+            // Add values, must use new
+            cars = new string[] { "Volvo", "BMW", "Ford" };
+            //method2
+            string[] cars1 = { "Volvo", "BMW", "Ford", "Mazda" };
+
+            // loop usage
+            foreach (string j in cars)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
 }
