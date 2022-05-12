68941eab657d25d698f699488abb99e9f52a1363
yuanqi <yuanqingxiao@gmail.com>
2022-05-11 17:36:50 -0700

    knowledge added

--- js/jsCode.js
+++ js/jsCode.js
@@ -14,3 +14,46 @@ console.log(fruits);
 let a=1,b="22";
 let c = `${a}${b}`;
 console.log(typeof c,c);//string,122
+
+/************************************************
+* closure->create private/static variable within a function
+************************************************/
+function makeCounter(){
+	var count = 0;
+	return function(){
+		return count++;
+	}
+}
+var count=makeCounter();
+console.log(count());
+console.log(count());
+console.log(count());
+/************************************************
+* string is immutable
+************************************************/
+
+str="abc";
+a=str.toUpperCase();
+a[0]=2;
+console.log(a);//a,ABC
+console.log(str)//str,abc
+
+/************************************************
+* Object usage
+************************************************/
+const Person ={firstName:"Luke",
+secondName:"Alex",
+age:19
+};
+console.log(Person.firstName);
+
+function Pson(fst,snd){
+	this.firstName=fst;
+	this.secondName=snd;
+}
+// why this? the firstName&secondName come from above
+Pson.prototype.getFullName=function (aaa){
+	return this.firstName+" GGG "+this.secondName+aaa;
+}
+var p1=new Pson("lll","kkk");
+console.log(p1.getFullName("abcddd"))
