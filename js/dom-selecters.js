557f4a0e005d4dfda5355cbe2b00be379dd28b89
yuanqi <yuanqingxiao@gmail.com>
2022-05-21 05:19:22 -0700

    js dom added

---
+++ js/dom-selecters.js
@@ -0,0 +1,26 @@
+// ==== dom selectors ====
+// [new way]
+// Good habbit: store the variables
+// default the first one
+let li_elem = document.querySelector("li");
+// select all elem, by html dom order
+let test = document.querySelectorAll("li,h1");
+console.log(test);
+let li_first_att = li_elem.getAttribute("random");
+li_elem.setAttribute("random", 1000);
+let li_first_newatt = li_elem.getAttribute("random");
+
+// [old way]
+// document.getElementById;
+// document.getElementsByClassName;
+
+// ==== change styles ====
+// classList method
+li_elem.classList.add("boldClass");
+test[2].classList.remove("big");
+test[2].classList.toggle("big");
+test[2].classList.toggle("big");
+
+li_elem.parentElement.setAttribute("id", "1");
+console.log(li_elem.parentElement.parentElement)
+// console.log(li_elem, test, li_first_att, li_first_newatt);
--- js/jsCode.js
+++ js/jsCode.js
@@ -1,137 +1,139 @@
 /************************************************
-* slice & splice difference
-************************************************/
+ * slice & splice difference
+ ************************************************/
 
 // slice->don't change original array, return [start,end)
-const fruits = ['Banana', 'Orange', 'Apple', 'Kiwi'];
+const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
 console.log(fruits.slice(2, 4));
 // splice->change original array, usage(startIndex,numIndex,replacedValue);numIndex==0?->insert
-fruits.splice(2, 0,'pear')
+fruits.splice(2, 0, "pear");
 console.log(fruits);
-fruits.splice(2, 2,'blueberry')
+fruits.splice(2, 2, "blueberry");
 console.log(fruits);
 
 /************************************************
-* closure->create private/static variable within a function
-************************************************/
-function makeCounter(){
-	var count = 0;
-	return function(){
-		return count++;
-	}
+ * closure->create private/static variable within a function
+ ************************************************/
+function makeCounter() {
+  var count = 0;
+  return function () {
+    return count++;
+  };
 }
-var count=makeCounter();
+var count = makeCounter();
 console.log(count());
 console.log(count());
 console.log(count());
 /************************************************
-* string is immutable
-************************************************/
+ * string is immutable
+ ************************************************/
 
-str="abc";
-a=str.toUpperCase();
-a[0]=2;
-console.log(a);//a,ABC
-console.log(str)//str,abc
+let str = "abc";
+let string_a = str.toUpperCase();
+// let a[0]=2;
+console.log(string_a); //a,ABC
+console.log(str); //str,abc
 
 /************************************************
-* Object usage
-************************************************/
-const Person ={firstName:"Luke",
-secondName:"Alex",
-age:19
-};
+ * Object usage
+ ************************************************/
+const Person = { firstName: "Luke", secondName: "Alex", age: 19 };
 console.log(Person.firstName);
 
-function Pson(fst,snd){
-	this.firstName=fst;
-	this.secondName=snd;
+function Pson(fst, snd) {
+  this.firstName = fst;
+  this.secondName = snd;
 }
 // why this? the firstName&secondName come from above
-Pson.prototype.getFullName=function (aaa){
-	return this.firstName+" GGG "+this.secondName+aaa;
-}
-var p1=new Pson("lll","kkk");
-console.log(p1.getFullName("abcddd"))
+Pson.prototype.getFullName = function (aaa) {
+  return this.firstName + " GGG " + this.secondName + aaa;
+};
+var p1 = new Pson("lll", "kkk");
+console.log(p1.getFullName("abcddd"));
 
+console.log("==== json/string convert ====");
+let json = { a: "A", b: "B" };
+let jsonString = JSON.stringify(json);
+let json_again = JSON.parse(jsonString);
+console.log(typeof json, typeof jsonString, typeof json_again); //object,string,object
 
-console.log("==== json/string convert ====")
-let json={"a":"A","b":"B"}
-let jsonString=JSON.stringify(json)
-let json_again=JSON.parse(jsonString)
-console.log(typeof json,typeof jsonString,typeof json_again)//object,string,object
-
-console.log("==== deconstruct ====")
+console.log("==== deconstruct ====");
 // !!!NEVER start a statement with ( or [
-let [x,y]=[1,2,3]
-console.log(x,y); // semicolon can't be ignored here
-[x,y]=[y,x] // swap value
-console.log(x,y)
-let nums=[1,2,3,4,5]
-let [a,b,...c]=nums
-console.log(a,b,c)
+let [x, y] = [1, 2, 3];
+console.log(x, y); // semicolon can't be ignored here
+[x, y] = [y, x]; // swap value
+console.log(x, y);
+let nums = [1, 2, 3, 4, 5];
+let [a, b, ...c] = nums;
+console.log(a, b, c);
 
 // all objects, can changed the property of the const variable, can't reassign value
-const sally={name:'Sally Lee'}
-sally.age=29 //OK
+const sally = { name: "Sally Lee" };
+sally.age = 29; //OK
 // sally={name:'Sally Alex'};//error
-console.log(sally)
+console.log(sally);
 
 // deconstruct object,way1,2,3 exactly the same
-let herry={name:"herry",age:18};
-({name,age}=herry)//way1,()is must,or it's the start of a block statement
+let herry = { name: "herry", age: 18 };
+({ name, age } = herry); //way1,()is must,or it's the start of a block statement
 // let {name,age} = herry;//way2
 // let {name:name,age:age}=herry;//way3
-console.log(name,age)
+console.log(name, age);
 
 // compute through brackets
-let field = 'Age'
-let harrysAge = sally[field.toLowerCase()]
-console.log(harrysAge) //29
+let field = "Age";
+let harrysAge = sally[field.toLowerCase()];
+console.log(harrysAge); //29
 
 // automatic convert
-const numbers=[1,2,,3,'many']
-console.log(numbers['1'],numbers[1],numbers[6])
+const numbers = [1, 2, , 3, "many"];
+console.log(numbers["1"], numbers[1], numbers[6]);
 
-console.log("==== use math.floor over bitwise ====")
+console.log("==== use math.floor over bitwise ====");
 //math.floor->,right; bitwise, since >=2**31 wrong.
-console.log(Math.floor(5/4))
-console.log((5/4)|0)
+console.log(Math.floor(5 / 4));
+console.log((5 / 4) | 0);
 
-console.log("==== for..of/in: const VS let ====")
+console.log("==== for..of/in: const VS let ====");
 // for..of/in: don't change in the NEXT loop
 // const can't be changed in CURRENT loop->safer
 // let can be changed in CURRENT loop
-for (let i of [2,3,4]) {
-    console.log(i)
-    i*=2
-    console.log('new i=',i)
+for (let i of [2, 3, 4]) {
+  console.log(i);
+  i *= 2;
+  console.log("new i=", i);
 }
 // for (const i of [2,3,4]) {
 //     console.log(i)
-//     i*=2 //i 
+//     i*=2 //i
 //     console.log('new i=',i)
 // }
 
 // forEach used more on array
 const numbes = [65, 44, 12, 4];
-numbes.forEach((ele,idx)=>{console.log(ele,idx)});
+numbes.forEach((ele, idx) => {
+  console.log(ele, idx);
+});
 
-const obj={name:'harry smith',age:19}
-obj.sex="male"
-for(const key in obj)
-    console.log(`${key}, ${obj[key]}`)
+const obj = { name: "harry smith", age: 19 };
+obj.sex = "male";
+for (const key in obj) console.log(`${key}, ${obj[key]}`);
 
 // Object.entries() takes an object like { a: 1, b: 2, c: 3 } and turns it into an array of key-value pairs: [ [ 'a', 1 ], [ 'b', 2 ], [ 'c', 3 ] ]
-Object.entries(obj).forEach(([k,v])=>{console.log(`${k},${v}`);});
+Object.entries(obj).forEach(([k, v]) => {
+  console.log(`${k},${v}`);
+});
 
-console.log("==== Template literal, backticks ====")
-let destination='world'
-let greeting =`Hello, ${destination}`
-console.log(greeting)
+console.log("==== Template literal, backticks ====");
+let destination = "world";
+let greeting = `Hello, ${destination}`;
+console.log(greeting);
 
-console.log("==== ++ ====")
-let counter = 0
-let riddle = counter++
-let enigma = ++counter
-console.log(riddle,enigma,counter)//022
+console.log("==== ++ ====");
+let counter = 0;
+let riddle = counter++;
+let enigma = ++counter;
+console.log(riddle, enigma, counter); //022
+
+console.log("==== add listener ====");
+// var btn = button.addEventListener;
